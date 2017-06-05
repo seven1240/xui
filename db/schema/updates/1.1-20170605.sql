af10bec173454866c40d2f5cbcdffd74e9d75696
Seven Du <dujinfang@gmail.com>
2017-06-05 12:13:40 +0800

    Doh

--- db/schema/updates/1.1-20170605.sql
+++ db/schema/updates/1.1-20170605.sql
@@ -1,3 +1,3 @@
-alter table users add auto_record BOOLEAN NOT NULL DEFAULT 0 CHECK(auto_record IN (0, 1, '0', '1')),;
-alter table routes add auto_record BOOLEAN NOT NULL DEFAULT 0 CHECK(auto_record IN (0, 1, '0', '1')),;
+alter table users add auto_record BOOLEAN NOT NULL DEFAULT 0 CHECK(auto_record IN (0, 1, '0', '1'));
+alter table routes add auto_record BOOLEAN NOT NULL DEFAULT 0 CHECK(auto_record IN (0, 1, '0', '1'));
 alter table media_files add processing_flag INTEGER DEFAULT 0;
