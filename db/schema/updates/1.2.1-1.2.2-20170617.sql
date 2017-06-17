5ba2ec81db6cac217cac735b5b438a5f4ca739a7
Seven Du <dujinfang@gmail.com>
2017-06-17 12:48:27 +0800

    update db ver to 1.2.2

--- db/schema/updates/1.2.1-1.2.2-20170617.sql
+++ db/schema/updates/1.2.1-1.2.2-20170617.sql
@@ -4,3 +4,4 @@ ALTER TABLE cdrs ADD network_port VARCHAR;
 CREATE INDEX cdrs_uuid ON cdrs(uuid);
 CREATE INDEX start_stamp ON cdrs(start_stamp);
 
+UPDATE dicts SET v = '1.2.2' WHERE realm = 'XUI' and k = 'DBVER';
