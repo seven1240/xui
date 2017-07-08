db0d2e0402f86b6e5a9122debddb5d10c056aaaa
Seven Du <dujinfang@x-y-t.cn>
2017-07-08 17:34:14 +0800

    add bus line sql

---
+++ db/scripts/line.sql
@@ -0,0 +1,5 @@
+alter table line add start_station varchar;
+alter table line add stop_station varchar;
+
+update line set start_station = (select stat_name from station where line_code = line.line_code and station_order = 1 limit 1);
+update line set start_station = (select stat_name from station where line_code = line.line_code and station_order = (select max(station_order) from station where line_code = line.line_code) limit 1);
