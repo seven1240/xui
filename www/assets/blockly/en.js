8d04d3c53b9f3699e8909479040a5f2eba554f96
gaofei <gaofei@xyt.cn>
2017-07-18 18:52:00 -0700

    add block about hang up

--- www/assets/blockly/en.js
+++ www/assets/blockly/en.js
@@ -429,6 +429,7 @@ Blockly.Msg.FS_BLOCK_IMPORTANCENUM = "Importance_number";
 Blockly.Msg.FS_BLOCK_ANNOUNCE = "Announce file";
 Blockly.Msg.FS_BLOCK_MUSIC = "Music file";
 Blockly.Msg.FS_BLOCK_RETURN = "Return to previous level";
+Blockly.Msg.FS_BLOCK_READY = "over";
 
 Blockly.Msg.tNow = "Now";
 Blockly.Msg.tDate = "Create Time";
