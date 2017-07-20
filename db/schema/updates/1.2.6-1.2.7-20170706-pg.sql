ed399fcdc78b1f08f0cdf41bb3363b693fd0700d
Seven Du <dujinfang@gmail.com>
2017-07-20 12:53:16 +0800

    fix sqlite updates

--- db/schema/updates/1.2.6-1.2.7-20170706-pg.sql
+++ db/schema/updates/1.2.6-1.2.7-20170706-pg.sql
@@ -17,28 +17,47 @@ CREATE TABLE subscriptions (
 
 CREATE UNIQUE INDEX subscriptions_realm_ref_id_user_id ON subscriptions (realm, ref_id, user_id);
 
-
-
-CREATE TRIGGER t_auto_sub_ticket AFTER INSERT ON tickets
+CREATE OR REPLACE FUNCTION auto_sub_ticket() RETURNS TRIGGER AS
+$$
+DECLARE
 BEGIN
-	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.id, NEW.user_id WHERE NEW.user_id IS NOT NULL;
+	INSERT INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.id, NEW.user_id WHERE NEW.user_id IS NOT NULL
+		ON CONFLICT DO NOTHING;
 
-	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.id, NEW.current_user_id WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id;
+	RETURN NULL;
 END;
+$$
+LANGUAGE plpgsql;
 
-CREATE TRIGGER t_auto_sub_ticket1 AFTER UPDATE ON tickets
+CREATE OR REPLACE FUNCTION auto_sub_ticket1() RETURNS TRIGGER AS
+$$
+DECLARE
 BEGIN
-	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.id, NEW.current_user_id WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id;
+	INSERT INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.id, NEW.current_user_id
+			WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id
+		ON CONFLICT DO NOTHING;
+	RETURN NEW;
 END;
+$$
+LANGUAGE plpgsql;
 
-CREATE TRIGGER t_auto_sub_ticket2 AFTER INSERT ON ticket_comments
+CREATE OR REPLACE FUNCTION auto_sub_ticket2() RETURNS TRIGGER AS
+$$
+DECLARE
 BEGIN
-	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.ticket_id, NEW.user_id WHERE NEW.user_id IS NOT NULL;
+	INSERT INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.ticket_id, NEW.user_id WHERE NEW.user_id IS NOT NULL
+		ON CONFLICT DO NOTHING;
+	RETURN NEW;
 END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER t_auto_sub_ticket AFTER INSERT ON tickets FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket();
+CREATE TRIGGER t_auto_sub_ticket1 AFTER UPDATE ON tickets FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket1();
+CREATE TRIGGER t_auto_sub_ticket2 AFTER INSERT ON ticket_comments FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket2();
 
 INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id) SELECT DISTINCT 'TICKET', id, user_id FROM tickets;
 INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id) SELECT DISTINCT 'TICKET', id, current_user_id FROM tickets;
--- db/schema/updates/1.2.6-1.2.7-20170706.sql
+++ db/schema/updates/1.2.6-1.2.7-20170706.sql
@@ -9,55 +9,33 @@ CREATE TABLE subscriptions (
 	recv_weapp_notification INTEGER DEFAULT 1,
 	recv_email_notification INTEGER DEFAULT 0,
 
-	created_epoch TIMESTAMP(0) DEFAULT now(),
-	updated_epoch TIMESTAMP(0) DEFAULT now(),
-	deleted_epoch TIMESTAMP(0)
+	created_epoch INTEGER DEFAULT (DATETIME('now', 'localtime')),
+	updated_epoch INTEGER DEFAULT (DATETIME('now', 'localtime')),
+	deleted_epoch INTEGER
 );
 
 CREATE UNIQUE INDEX subscriptions_realm_ref_id_user_id ON subscriptions (realm, ref_id, user_id);
 
-CREATE OR REPLACE FUNCTION auto_sub_ticket() RETURNS TRIGGER AS
-$$
-DECLARE
+CREATE TRIGGER t_auto_sub_ticket AFTER INSERT ON tickets
 BEGIN
-	INSERT INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.id, NEW.user_id WHERE NEW.user_id IS NOT NULL
-		ON CONFLICT DO NOTHING;
+	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.id, NEW.user_id WHERE NEW.user_id IS NOT NULL;
 
-	RETURN NULL;
+	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.id, NEW.current_user_id WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id;
 END;
-$$
-LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION auto_sub_ticket1() RETURNS TRIGGER AS
-$$
-DECLARE
+CREATE TRIGGER t_auto_sub_ticket1 AFTER UPDATE ON tickets
 BEGIN
-	INSERT INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.id, NEW.current_user_id
-			WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id
-		ON CONFLICT DO NOTHING;
-	RETURN NEW;
+	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.id, NEW.current_user_id WHERE NEW.current_user_id IS NOT NULL AND NEW.user_id <> NEW.current_user_id;
 END;
-$$
-LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION auto_sub_ticket2() RETURNS TRIGGER AS
-$$
-DECLARE
+CREATE TRIGGER t_auto_sub_ticket2 AFTER INSERT ON ticket_comments
 BEGIN
-	INSERT INTO subscriptions (realm, ref_id, user_id)
-		SELECT 'TICKET', NEW.ticket_id, NEW.user_id WHERE NEW.user_id IS NOT NULL
-		ON CONFLICT DO NOTHING;
-	RETURN NEW;
+	INSERT OR IGNORE INTO subscriptions (realm, ref_id, user_id)
+		SELECT 'TICKET', NEW.ticket_id, NEW.user_id WHERE NEW.user_id IS NOT NULL;
 END;
-$$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER t_auto_sub_ticket AFTER INSERT ON tickets FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket();
-CREATE TRIGGER t_auto_sub_ticket1 AFTER UPDATE ON tickets FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket1();
-CREATE TRIGGER t_auto_sub_ticket2 AFTER INSERT ON ticket_comments FOR EACH ROW EXECUTE PROCEDURE auto_sub_ticket2();
-
 
 INSERT INTO subscriptions (realm, ref_id, user_id) SELECT DISTINCT 'TICKET', id, user_id FROM tickets;
 INSERT INTO subscriptions (realm, ref_id, user_id) SELECT DISTINCT 'TICKET', id, current_user_id FROM tickets;
