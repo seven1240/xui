623290e6caf025e587c090b223bfc759e1bc2895
Seven Du <dujinfang@gmail.com>
2017-07-21 13:40:46 +0800

    update update sql

--- db/schema/updates/1.2.9-1.3.0-20170720.sql
+++ db/schema/updates/1.2.9-1.3.0-20170720.sql
@@ -1,4 +1,5 @@
 ALTER TABLE tickets ALTER privacy SET DEFAULT 'TICKET_PRIV_PRIVATE';
+-- UPDATE tickets SET privacy = 'TICKET_PRIV_PRIVATE';
 ALTER TABLE tickets ADD rate VARCHAR;
 
 UPDATE dicts SET v = '1.3.0' WHERE realm = 'XUI' and k = 'DBVER';
