598f3a6319567f05ca0f79082c143ed135654deb
gaofei <gaofei@xyt.cn>
2017-08-30 01:58:24 -0700

    add voicemail.lua and block

---
+++ lua/xui/controller/voicemail.lua
@@ -0,0 +1,57 @@
+--[[
+/*
+ * HTML5 GUI Framework for FreeSWITCH - XUI
+ * Copyright (C) 2015-2017, Seven Du <dujinfang@x-y-t.cn>
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is XUI - GUI for FreeSWITCH
+ *
+ * The Initial Developer of the Original Code is
+ * Seven Du <dujinfang@x-y-t.cn>
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Seven Du <dujinfang@x-y-t.cn>
+ *
+ *
+ */
+]]
+
+xtra.start_session()
+xtra.require_login()
+
+content_type("application/json")
+require 'xdb'
+xdb.bind(xtra.dbh)
+
+post('/',function(params)
+		user = params.request.user
+		domain = params.request.domain
+		u = xdb.find_one("users", {extn = user})
+		if u then
+
+			local e = freeswitch.Event("MESSAGE_QUERY");
+			e:addHeader("Message-Account", user .. "@" .. domain)
+			e:addHeader("VM-Sofia-Profile", "default")
+			e:fire()
+		
+			return 200, {code = 200, text="OK"}
+		else
+			return 404, {code = 404, text="NOT FOUND"}
+		end		
+end)
+
+
