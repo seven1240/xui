942498a9f89f73677879882629e61122109c3b91
zhangyiwen <zhangyiwen@x-y-t.com>
2017-09-25 16:49:57 +0800

    fix warning

--- jsapp/src/jsx/libs/xtools.js
+++ jsapp/src/jsx/libs/xtools.js
@@ -128,7 +128,7 @@ class Notice extends React.Component {
 		if (this.state.level == 'error') class_name = 'error';
 
 		if (this.state.max > 0 && this.state.msgs.length > 0) {
-			return <NavDropdown id="notifications" key="notifications" title="notifications" eventKey="notifications" title={this.state.msg}>
+			return <NavDropdown id="notifications" key="notifications" eventKey="notifications" title={this.state.msg||""}>
 				{
 					this.state.msgs.map((msg, i)=> {
 						return <MenuItem key={i} eventKey={i} id={i}>{msg.msg}</MenuItem>
