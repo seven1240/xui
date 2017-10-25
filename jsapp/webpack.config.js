2599bc774e531dd9f86e4cb55aa841f4a58d618e
zhangyiwen <zhangyiwen@x-y-t.com>
2017-10-25 14:50:37 +0800

    still fix webpak

--- jsapp/webpack.config.js
+++ jsapp/webpack.config.js
@@ -1,40 +1,43 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var WebpackMd5Hash = require('webpack-md5-hash');
+var path = require('path');
+
 var config = {
     entry: {
         "index": ["./src/jsx/index.js", "./src/css/xui.css", "./src/css/dashboard.css"]
     },
 
     output: {
-        path: '../www/assets',
+        path: path.resolve(__dirname, '../www/assets'),
         filename: 'js/jsx/[name].[chunkhash:8].js',
         publicPath: '/assets'
     },
 
     module: {
-        loaders: [{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
-            exclude: /node_modules/
-        }, {
-            test: /\.(js|jsx)$/,
-            loaders: ['react-hot', 'babel?' + JSON.stringify({
-                cacheDirectory: true,
-                plugins: [
-                    'transform-runtime',
-                    'transform-decorators-legacy'
-                ],
-                presets: ['es2015', 'react', 'stage-0'],
-                env: {
-                    production: {
-                        presets: ['react-optimize']
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract(['css-loader'])
+            }, {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: [{loader: "react-hot-loader"}, {loader: 'babel-loader?' + JSON.stringify({
+                        plugins: [
+                            'transform-runtime',
+                            'transform-decorators-legacy'
+                        ],
+                        presets: ['es2015', 'react', 'stage-0'],
+                        env: {
+                                production: {
+                                    presets: ['react-optimize']
+                                }
+                        }
                     }
-                }
-            })],
-            exclude: /node_modules/
-        }]
+                )}
+                ]
+            }
+        ]
     },
 
     performance: {
@@ -48,9 +51,6 @@ var config = {
             inject: true,
             chunks: ['index']
         }),
-
-
-        new WebpackMd5Hash(),
 
         new ExtractTextPlugin("css/xui.[contenthash:8].css"),
     ]
--- jsapp/webpack.production.config.js
+++ jsapp/webpack.production.config.js
@@ -1,48 +1,51 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var WebpackMd5Hash = require('webpack-md5-hash');
 var config = {
     entry: {
         "react": ["react", "react-dom", "react-router", "react-bootstrap", "i18n-react"],
         "index": ["./src/jsx/index.js", "./src/css/xui.css", "./src/css/dashboard.css"]
     },
 
-    noresolve: {
+/*
+    resolve: {
         alias: {
             'react': 'react-lite',
             'react-dom': 'react-lite'
         }
     },
+*/
 
     output: {
-        path: '../www/assets',
+        path: __dirname + '/../www/assets',
         filename: 'js/jsx/[name].[chunkhash:8].js',
         publicPath: '/assets'
     },
 
     module: {
-        loaders: [{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
-            exclude: /node_modules/
-        }, {
-            test: /\.(js|jsx)$/,
-            loaders: ['react-hot', 'babel?' + JSON.stringify({
-                cacheDirectory: true,
-                plugins: [
-                    'transform-runtime',
-                    'transform-decorators-legacy'
-                ],
-                presets: ['es2015', 'react', 'stage-0'],
-                env: {
-                    production: {
-                        presets: ['react-optimize']
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract(['css-loader'])
+            }, {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: [{loader: "react-hot-loader"}, {loader: 'babel-loader?' + JSON.stringify({
+                        plugins: [
+                            'transform-runtime',
+                            'transform-decorators-legacy'
+                        ],
+                        presets: ['es2015', 'react', 'stage-0'],
+                        env: {
+                                production: {
+                                    presets: ['react-optimize']
+                                }
+                        }
                     }
-                }
-            })],
-            exclude: /node_modules/
-        }]
+                )}
+                ]
+            }
+        ]
     },
 
     performance: {
@@ -50,16 +53,19 @@ var config = {
     },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin("react", "js/react.15.4.1.bundle.js"),
+        new webpack.optimize.CommonsChunkPlugin({name: "react", filename: "js/react.15.4.1.bundle.js"}),
 
         new HtmlWebpackPlugin({
             filename: '../index.html',
             template: './index.html',
             inject: true,
-            chunks: ['react', 'index']
+            chunks: ['react', 'index'],
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            }
         }),
 
-        new WebpackMd5Hash(),
 
         new ExtractTextPlugin("./css/xui.[chunkhash:8].css"),
 
