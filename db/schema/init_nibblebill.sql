14c350847037d29a7f83571f96e2eb991a29b03c
jiangdeyong <jiangdeyong@x-y-t.cn>
2017-11-20 02:40:47 +0800

    aaa

---
+++ db/schema/init_nibblebill.sql
@@ -0,0 +1,12 @@
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'odbc-dsn', 'bandwidth.com', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'db_table', 'accounts', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'db_column_cash', 'cash', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'db_column_account', 'id', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'global_heartbeat', '60', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'lowbal_amt', '5', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'lowbal_action', 'play ding', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'nobal_amt', '0', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'nobal_action', 'hangup', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'percall_max_amt', '100', 0, 0);
+INSERT INTO params (realm, k, v, ref_id, disabled) VALUES('NIBBLE', 'percall_action', 'hangup', 0, 0);
+
