a1627a5e8190add22677eeba4a2f66e316ef15c2
jiangxueyun <jiangxueyun@x-y-t.cn>
2017-11-22 10:42:38 +0800

    mod_opus

--- db/schema/init_settings.sql
+++ db/schema/init_settings.sql
@@ -8,3 +8,14 @@ INSERT INTO params (realm, k, v, disabled) VALUES('event_socket', 'password', 'C
 INSERT INTO params (realm, k, v, disabled) VALUES('event_socket', 'apply-inbound-acl', 'loopback.auto', 1);
 INSERT INTO params (realm, k, v, disabled) VALUES('event_socket', 'password', 'ClueCon', 0);
 INSERT INTO params (realm, k, v, disabled) VALUES('event_socket', 'stop-on-bind-error', 'true', 1);
+
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'use-vbr', '1', 0);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'complexity', '10', 0);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'keep-fec-enabled', '1', 0);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'maxaveragebitrate', '0', 0);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'maxplaybackrate', '0', 0);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'use-dtx', '10', 1);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'packet-loss-percent', '10', 1);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'asymmetric-sample-rates', '0', 1);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'use-jb-lookahead', 'true', 1);
+INSERT INTO params (realm, k, v, disabled) VALUES('opus', 'sprop-maxcapturerate', '0', 1);
