4aaf3fc94b19b28dd26e1f89fe4ddeb5d092ae2a
Seven Du <dujinfang@x-y-t.cn>
2018-03-11 21:23:10 +0800

    rename to avoid auto init

---
+++ db/schema/ticket_init_dict.sql
@@ -0,0 +1,5 @@
+INSERT INTO dicts (realm, k, v) VALUES ('TICKET_TYPE', 'TICKET_TYPE_1', '一般问题');
+INSERT INTO dicts (realm, k, v) VALUES ('TICKET_TYPE', 'TICKET_TYPE_2', '上班打卡');
+INSERT INTO dicts (realm, k, v) VALUES ('TICKET_TYPE', 'TICKET_TYPE_3', '通知提醒');
+INSERT INTO dicts (realm, k, v) VALUES ('TICKET_TYPE', 'TICKET_TYPE_4', '投诉建议');
+INSERT INTO dicts (realm, k, v) VALUES ('TICKET_TYPE', 'TICKET_TYPE_5', '其它');
